1e78fab4efbb0e4a2702d8d339108f68f1627b19
GiftiaCoder <yuechengzi1@gmail.com>
2018-03-26 00:50:29 +0800

    use system standard defs

--- src/basic_defs.h
+++ src/basic_defs.h
@@ -2,19 +2,14 @@
 #ifndef _BASIC_DEFS_H_
 #define _BASIC_DEFS_H_
 
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
 
-#define NUL(_type_) ((_type_)0)
+#include <errno.h>
 
-#define SUCCESS (0)
-#define PASS (0)
-#define OK (0)
-#define FAIL (-1)
-#define ERROR (-1)
-
-typedef int abool; // a bool type in aira system
-#define TRUE (1)
-#define FALSE (0)
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #endif
